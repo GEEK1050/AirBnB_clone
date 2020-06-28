1aa34517cf42897ca9eb62e03676b5e9b47b923f
karim bahri <karim-bahri-18@outlook.fr>
2020-06-28 18:53:46 +0000

    reload

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
 
@@ -24,13 +24,13 @@ class BaseModel:
         self.updated_at = datetime.now()
         for key, value in kwargs.items():
             if key is "created_at":
-                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                self.created_at = value.isoformat()
+                self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
             elif key is "updated_at":
-                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                self.updated_at = value.isoformat()
-            if key is not "__class__":
+                self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            if key != "__class__":
                 setattr(self, key, value)
+            else:
+                self.__class__.__name__ = value
 
     def __str__(self):
         """returns an update for the BaseModel"""
@@ -39,14 +39,16 @@ class BaseModel:
     def save(self):
         """returns an update for the updated_at"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
         
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values"""
         model_dict = {}
         assign_dict(model_dict, self.__dict__)
-        model_dict['created_at'] = self.created_at.isoformat()
-        model_dict['updated_at'] = self.updated_at.isoformat()
+        model_dict['updated_at'] = str(self.updated_at)
+        model_dict['created_at'] = str(self.created_at)
         model_dict["__class__"] = self.__class__.__name__
         return model_dict
 
@@ -54,4 +56,5 @@ class BaseModel:
 def assign_dict(dest, src):
     """assign the value of src to dest"""
     for key, value in src.items():
-        dest[key] = value
+        if key != 'created_at' and key != "updated_at":
+            dest[key] = value
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,11 +3,12 @@
 
 import json
 from datetime import datetime
+from models.base_model import BaseModel
 
 class FileStorage:
     """serialize instances to a JSON file"""
 
-    __file_path = ""
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -16,23 +17,27 @@ class FileStorage:
 
     def new(self, obj):
         """set in __objects"""
-        self.__objects["{}.{}".format(self.__class__.__name__, obj.id)] = obj
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        with open(self.__file_path, 'w', encoding="utf-8") as my_file:
+        with open(self.__file_path, "w", encoding="utf-8") as my_file:
             my_dict = {}
             for key, value in self.__objects.items():
                 my_dict[key] = value.to_dict()
-            js_object = json.dump(my_dict)
+            js_object = json.dumps(my_dict)
             my_file.write(js_object)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, "r") as my_file:
-                my_dict = json.load(my_file)
+                dict_js = my_file.read()
+                my_dict = json.loads(dict_js)
                 for key, value in my_dict.items():
-                    self.__objects[key] = value
+                    obj = eval(value["__class__"])
+                    self.__objects[key] = obj(**value)
+                    print("reload here")
+                    self.__objects["{}.{}".format(self.__class__.__name__, obj.id)] = obj
         except:
             return
