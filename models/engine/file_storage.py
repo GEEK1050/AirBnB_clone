f90b798d17a5c5155580c67853f2e4e5a969f1ee
Boul3ez85 <benrebah.moez@gmail.com>
2020-07-01 12:58:27 +0100

    fix import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,12 @@ import os
 from datetime import datetime
 from models.base_model import BaseModel
 import copy
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
